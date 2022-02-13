56d4184cfff98126b9ff7af9f3a5d51c97fe8d6a
Pavel Petkov <elias5247698@abv.bg>
2022-02-14 01:10:59 +0200

    making sidenav

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Route, Routes} from "react-router-dom"
 import Login from "./components/Login";
 import Register from "./components/Register";
+import SideNav from "./components/SideNav";
 
 function App() {
     return (
         <div className="App">
-            <div className={"auth-wrapper"}>
-                <div className={"auth-inner"}>
-                    <Routes>
-                    <Route element={<Login/>} path={"/login"}/>
-                    <Route element={<Register/>} path={"/register"}/>
-                    </Routes>
-                </div>
-            </div>
+            <SideNav />
+            <Routes>
+                <Route element={<Login/>} path={"/login"}/>
+                <Route element={<Register/>} path={"/register"}/>
+            </Routes>
 
         </div>
     );
