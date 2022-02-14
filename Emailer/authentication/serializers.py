5d2f44d0bb9de2d654552a10999d781737df718a
Pavel Petkov <elias5247698@abv.bg>
2022-02-14 06:17:59 +0200

    not needed login serializer and view[they are build in in jwt sign logic]

--- Emailer/authentication/serializers.py
+++ Emailer/authentication/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[
+            UniqueValidator(queryset=User.objects.all())
+        ]
+    )
+
     class Meta:
         fields = ("username", "last_name", "email", "password",)
-        model = User
-
-
-class LoginSerializer(serializers.ModelSerializer):
-
-
-    class Meta:
-        fields = ("email", "password")
         model = User
--- Emailer/authentication/urls.py
+++ Emailer/authentication/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt import views
 
-from Emailer.authentication.views import LoginAPIView, RegisterAPIView
+from Emailer.authentication.views import  RegisterAPIView
 
 urlpatterns = [
     path("refresh", views.TokenObtainPairView.as_view()),
     path("", views.TokenRefreshView.as_view()),
-    path("login", LoginAPIView.as_view()),
     path("register", RegisterAPIView.as_view()),
 ]
