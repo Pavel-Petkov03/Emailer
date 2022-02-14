938c1c6f9b36d33803f363acb6b4238744d791b9
Pavel Petkov <elias5247698@abv.bg>
2022-02-14 05:23:15 +0200

    making form names to be retrieved from form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -19,17 +19,11 @@ function Login() {
                     <h3>Sign In</h3>
                     <div className="form-group">
                         <label>Email address</label>
-                        <input type="email" className="form-control" placeholder="Enter email"/>
+                        <input type="email" className="form-control" placeholder="Enter email" name={"email"}/>
                     </div>
                     <div className="form-group">
                         <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Enter password"/>
-                    </div>
-                    <div className="form-group">
-                        <div className="custom-control custom-checkbox">
-                            <input type="checkbox" className="custom-control-input" id="customCheck1"/>
-                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                        </div>
+                        <input type="password" className="form-control" placeholder="Enter password"  name={"password"}/>
                     </div>
                     <button onClick={onClick} type="submit" className="btn btn-primary btn-block">Submit</button>
                     <p className="forgot-password text-right">
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,6 +1,7 @@
 import "../css/auth.css"
 import "react-pro-sidebar"
 function Register() {
+    // I type names in snake case because Django gets them and directly puts them in User model
     return (
         <div className={"auth-wrapper"}>
             <div className={"auth-inner"}>
@@ -8,23 +9,23 @@ function Register() {
                     <h3>Sign Up</h3>
                     <div className="form-group">
                         <label>First name</label>
-                        <input type="text" className="form-control" placeholder="First name"/>
+                        <input type="text" className="form-control" placeholder="First name" name={"first_name"}/>
                     </div>
                     <div className="form-group">
                         <label>Last name</label>
-                        <input type="text" className="form-control" placeholder="Last name"/>
+                        <input type="text" className="form-control" placeholder="Last name" name={"last_name"}/>
                     </div>
                     <div className="form-group">
                         <label>Email address</label>
-                        <input type="email" className="form-control" placeholder="Enter email"/>
+                        <input type="email" className="form-control" placeholder="Enter email" name={"email"}/>
                     </div>
                     <div className="form-group">
                         <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Enter password"/>
+                        <input type="password" className="form-control" placeholder="Enter password" name={"password"}/>
                     </div>
                     <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
                     <p className="forgot-password text-right">
-                        Already registered <a href="#">sign in?</a>
+                        Already registered <a href={"/login"}>sign in?</a>
                     </p>
                 </form>
             </div>
