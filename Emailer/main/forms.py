3bb12e5668217b1516bd593ab33315eee5978664
Pavel Petkov <elias5247698@abv.bg>
2022-03-02 10:29:02 +0200

    making custom user model

--- Emailer/main/forms.py
+++ Emailer/main/forms.py
@@ -2,18 +2,10 @@ from django.contrib.auth.models import User
 from django import forms
 
 
-class CustomAuthForm(forms.ModelForm):
-    class Meta:
-        abstract = True
-
-
 class RegisterForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     class Meta:
         model = User
-        fields = ("username" , "password", "confirm_password")
+        fields = ("username", "password", "confirm_password")
         widgets = {
             "username": forms.TextInput(attrs={
                 "type": "text", "class": 'form-control', "placeholder": "Enter Username"
@@ -26,19 +18,8 @@ class RegisterForm(forms.ModelForm):
             }),
         }
 
-# <form class="custom-form">
-#                <h3>Sign In</h3>
-#                <div class="form-group">
-#                    <label>Enter Username</label>
-#                    <input type="email" class="form-control" placeholder="Enter username"/>
-#                </div>
-#
-#                <div class="form-group">
-#                    <label>Password</label>
-#                    <input type="password" class="form-control" placeholder="Enter password"/>
-#                </div>
-#                <button type="submit" class="btn btn-primary btn-block">Submit</button>
-#                <p class="forgot-password text-right">
-#                    Forgot <a href="#">password?</a>
-#                </p>
-#            </form>
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("username", "password")
