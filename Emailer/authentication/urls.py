237074d705e9c240826940cfb7e0918d21125c08
Pavel Petkov <elias5247698@abv.bg>
2022-03-02 08:44:51 +0200

    making auth template with django implementation

--- Emailer/authentication/urls.py
+++ Emailer/authentication/urls.py
@@ -1,12 +1,1 @@
-from django.urls import path
-from rest_framework_simplejwt import views
-from .views import RegisterAPIView
 
-
-
-
-urlpatterns = [
-    path("login", views.TokenObtainPairView.as_view()),
-    path("refresh", views.TokenRefreshView.as_view()),
-    path("register", RegisterAPIView.as_view()),
-]
--- Emailer/authentication/views.py
+++ Emailer/authentication/views.py
@@ -1,24 +1,3 @@
-import django.views.generic
-from rest_framework import status
-from rest_framework.authentication import SessionAuthentication
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from .serializers import RegistrationSerializer
-from rest_framework import generics
 
-
-class SessionCsrfExemptAuthentication(SessionAuthentication):
-    def enforce_csrf(self, request):
-        pass
-
-
-class RegisterAPIView(generics.GenericAPIView, generics.mixins.CreateModelMixin):
-    authentication_classes = (SessionCsrfExemptAuthentication,)
-    permission_classes = (AllowAny,)
-    serializer_class = RegistrationSerializer
-
-    def post(self, request, *args, **kwargs):
-        super().create(request, *args, **kwargs)
 
 
