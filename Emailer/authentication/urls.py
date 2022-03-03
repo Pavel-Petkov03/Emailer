975f6f441da7cdc1a8bd14ef7d4a5e9c17558f77
Pavel Petkov <elias5247698@abv.bg>
2022-03-03 21:33:49 +0200

    making css for the app

--- Emailer/authentication/urls.py
+++ Emailer/authentication/urls.py
@@ -1,8 +1,15 @@
+from django.shortcuts import render
 from django.urls import path
 
 from Emailer.authentication.views import LoginView, RegisterView
 
+def test(req):
+    return render(req, "table.html")
+
 urlpatterns = [
     path("login", LoginView.as_view(), name="login"),
-    path("register", RegisterView.as_view(), name="register")
+    path("register", RegisterView.as_view(), name="register"),
+    path("table" , test)
 ]
+
+
