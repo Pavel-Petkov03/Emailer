541f5bea136201919843ae6693aee041007ae807
Pavel Petkov <elias5247698@abv.bg>
2022-03-10 00:09:17 +0200

    cors not needed to fetch from same server

--- Emailer/settings.py
+++ Emailer/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,10 +51,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
 ]
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8000',
-)
 
 ROOT_URLCONF = 'Emailer.urls'
 
