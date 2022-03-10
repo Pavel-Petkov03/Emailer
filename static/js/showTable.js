532a47784bc5dcfbacba2ef1ae9ec5695fa65b0f
Pavel Petkov <elias5247698@abv.bg>
2022-03-10 02:33:46 +0200

    adding frontend logic

--- static/js/showTable.js
+++ static/js/showTable.js
@@ -1,9 +1,24 @@
 import {endpoints, getData} from "./api.js";
 
-
+const i = document.createElement("i")
+i.className = "fa fa-solid fa-filter"
 window.onload = async () => {
-    let data = await getData(endpoints.folder)
+    let filter = localStorage.getItem("filter") || ""
+    await loadRows(endpoints.folder + "?" + filter)
+    let headerTrs = document.querySelectorAll("thead tr th")
+    Array.from(headerTrs).forEach(el => {
+        if (el.textContent.toLowerCase() === localStorage.getItem("filter")) {
+            el.prepend(i)
+        }
+        el.addEventListener("click", filterEvent)
+    })
+}
+
+
+async function loadRows(url) {
+    let data = await getData(url)
     let tbody = document.querySelector("tbody")
+    tbody.innerText = ""
     data.forEach(el => {
         let tr = document.createElement("tr")
         Object.values(el).forEach(value => {
@@ -15,3 +30,10 @@ window.onload = async () => {
     })
 }
 
+
+async function filterEvent(ev) {
+    ev.target.prepend(i)
+    const loweredFilter = ev.target.textContent.toLowerCase()
+    localStorage.setItem("filter", loweredFilter)
+    await loadRows(endpoints.folder + "?" + loweredFilter)
+}
